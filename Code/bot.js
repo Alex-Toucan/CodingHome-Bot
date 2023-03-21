d502682ec757996cee962d417b2f96d1b1587e6a
Venen Jean <102526504+Savageboitim13@users.noreply.github.com>
2023-03-21 11:18:17 +0100

    Update bot.js

--- Code/bot.js
+++ Code/bot.js
@@ -1,1 +1,9 @@
-Console.log("CodingHome Bot");
+import { Client } from "discord.js";
+
+console.log("Bot is starting...");
+
+const client = new Client({
+    intents: []
+});
+
+console.log(client);
