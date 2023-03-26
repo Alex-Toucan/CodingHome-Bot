98a9c0cd60ebb31331f7146ba4aafb899d60072b
Venen Jean <102526504+Savageboitim13@users.noreply.github.com>
2023-03-26 09:51:16 +0200

    Update staffManager.ts

--- src/StaffFunctions/staffManager.ts
+++ src/StaffFunctions/staffManager.ts
@@ -1,4 +1,4 @@
-// staffManager.ts / handles all the available functions of the server team
+// staffManager.ts / handles all the available functions of the server team.
 // Concept: index.ts -> invokes function of staffManager.ts ->
 // invokes the function of the specified file -> function returns the result to staffManager.ts ->
 // staffManager.ts returns result to index.ts -> index.ts handles the result
