eb331f2256e4fd1a9d32c3a14152918f29a3045e
Venen Jean <102526504+Savageboitim13@users.noreply.github.com>
2023-03-26 09:49:28 +0200

    Create staffManager.ts

---
+++ src/StaffFunctions/staffManager.ts
@@ -0,0 +1,8 @@
+// staffManager.ts / handles all the available functions of the server team
+// Concept: index.ts -> invokes function of staffManager.ts ->
+// invokes the function of the specified file -> function returns the result to staffManager.ts ->
+// staffManager.ts returns result to index.ts -> index.ts handles the result
+
+// only specific roles will have access to these functions.
+
+// staffManager.ts and functions'll be in the "StaffFunctions" directory.
