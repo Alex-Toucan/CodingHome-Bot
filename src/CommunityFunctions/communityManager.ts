4795b64ba4200118be5e44c9b0cc3c32d95417b7
Venen Jean <102526504+Savageboitim13@users.noreply.github.com>
2023-03-26 09:46:30 +0200

    Update communityManager.ts

--- src/CommunityFunctions/communityManager.ts
+++ src/CommunityFunctions/communityManager.ts
@@ -2,3 +2,5 @@
 // Concept: index.ts -> invokes function of communityManager.ts ->
 // invokes the function of the specified file -> function returns the result to communityManager.ts ->
 // communityManager.ts return result to index.ts -> index.ts handles the result
+
+// communiryManager.ts and functions'll be in the "CommunityFunctions" directory.
