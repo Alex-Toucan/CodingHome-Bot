8ccbc8119a72119321afc33fb5792c6564716587
Venen Jean <102526504+Savageboitim13@users.noreply.github.com>
2023-03-26 09:44:57 +0200

    Delete CommunityFunctions directory

--- CommunityFunctions/communityManager.ts
+++
@@ -1,4 +0,0 @@
-// "Community Functionality" manager / handles all the available functions
-// Concept: index.ts -> invokes function of communityManager.ts ->
-// invokes the function of the specified file -> function returns the result to communityManager.ts ->
-// communityManager.ts return result to index.ts -> index.ts handles the result
